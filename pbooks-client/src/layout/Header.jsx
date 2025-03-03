bd8d5afdcf3e3b2b478b92e8a4f8dc5b468edea7
bakakoozin <cyrilpebre@gmail.com>
2025-03-03 22:49:51 +0100

    gestion affichage avatar

--- pbooks-client/src/layout/Header.jsx
+++ pbooks-client/src/layout/Header.jsx
@@ -1,7 +1,7 @@
 import { useDispatch, useSelector } from "react-redux";
 import { NavLink, useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faCircleUser } from "@fortawesome/free-solid-svg-icons";
 
 import { toggleMenu } from "../features/menuSlice";
 import { logout } from "../features/authSlice";
@@ -10,6 +10,7 @@ import { API_URL } from "../utils/constants";
 function Header() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const { infos } = useSelector((state) => state.auth);
   const { isLogged, pseudo } = useSelector((state) => state.auth);
   const { isMenuOpen } = useSelector((state) => state.menu);
 
@@ -28,10 +29,6 @@ function Header() {
   function handleClick() {
     dispatch(toggleMenu());
   }
-
-  //  function handleAvatar(user) {
-  //     return user.cover_url ? `/medias/${user.avatar}` : notFoundAvatar;
-  //   }
 
   return (
     <header className="header">
@@ -58,7 +55,11 @@ function Header() {
         <FontAwesomeIcon icon={faBars} />
       </div>
       <div className="user-infos">
-      {/* <img src={handleAvatar(user)} alt="avatar" /> */}
+        {!isLogged || (infos.avatar === null) ? (
+          <FontAwesomeIcon icon={faCircleUser} />
+        ) : (
+          <img src={infos.avatar} alt={pseudo} />
+        )}
         <p>{isLogged ? pseudo : "non connecté"}</p>
       </div>
     </header>
