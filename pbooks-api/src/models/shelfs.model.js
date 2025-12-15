63409a4cfa8d0b0871e6c81db59133202821532c
bakakoozin <cyrilpebre@gmail.com>
2025-12-15 09:15:20 +0100

    correctif status shelf user v2

--- pbooks-api/src/models/shelfs.model.js
+++ pbooks-api/src/models/shelfs.model.js
@@ -96,7 +96,7 @@ class Shelf {
       LEFT JOIN authors ON authors.id = volumes_authors.authors_id
       LEFT JOIN medias ON medias.volumes_id = volumes.id
       LEFT JOIN shelfs ON shelfs.volumes_id = volumes.id AND shelfs.users_id = ?
-      WHERE shelfs.users_id = ? AND works.id = ?
+      WHERE works.id = ?
       GROUP BY volumes.id, works.id, users.id, medias.url
       ORDER BY vol_num;`; // Trie par numéro de volume
     return await pool.query(FIND_ONE_WORK, [users_id, works_id]);
