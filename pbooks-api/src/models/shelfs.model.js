9d33ff65f4933f7619df41df8254f9f5b6d3ddf8
bakakoozin <cyrilpebre@gmail.com>
2025-12-15 12:49:24 +0100

    test correctif shelf status v1.2

--- pbooks-api/src/models/shelfs.model.js
+++ pbooks-api/src/models/shelfs.model.js
@@ -10,94 +10,31 @@ class Shelf {
     // Requête pour récupérer les ouvrages avec leurs informations
     const FIND_ALL_WORKS = `
       SELECT 
-        works.id AS works_id, 
-        works.name AS works_name,
-        works.edition AS works_edition,
-        works.type AS works_type,
-        works.format AS works_format,
-        COALESCE(GROUP_CONCAT(DISTINCT authors.name SEPARATOR ','), 'Inconnu') AS authors_name,
-        (
-          SELECT medias.url 
-          FROM volumes
-          LEFT JOIN medias ON medias.volumes_id = volumes.id
-          WHERE volumes.works_id = works.id
-          ORDER BY volumes.number ASC
-          LIMIT 1
-        ) AS cover_url
-      FROM works
-      LEFT JOIN volumes ON volumes.works_id = works.id
-      LEFT JOIN volumes_authors ON volumes_authors.volumes_id = volumes.id
-      LEFT JOIN authors ON authors.id = volumes_authors.authors_id
-      LEFT JOIN shelfs ON shelfs.volumes_id = volumes.id
-      WHERE shelfs.users_id = ?
-      AND (
-        works.name LIKE CONCAT('%', ?, '%')
-        OR volumes.title LIKE CONCAT('%', ?, '%')
-      )
-      GROUP BY 
-        works.id,
-        works.name,
-        works.edition,
-        works.type,
-        works.format
-      LIMIT ${limit} OFFSET ${offset}
-    `;
-
-    // Requête pour compter le nombre total d'ouvrages correspondant à la recherche
-    const COUNT_QUERY = `
-      SELECT COUNT(DISTINCT works.id) AS total
-      FROM works
-      LEFT JOIN volumes ON volumes.works_id = works.id
-      LEFT JOIN shelfs ON shelfs.volumes_id = volumes.id
-      WHERE shelfs.users_id = ?
-      AND (
-        works.name LIKE CONCAT('%', ?, '%')
-        OR volumes.title LIKE CONCAT('%', ?, '%')
-      )
-    `;
-
-    const params = [user_id, search, search]; // Paramètres pour les requêtes
-
-    // Exécute les requêtes
-    const [datas] = await pool.query(FIND_ALL_WORKS, params);
-    const [countResult] = await pool.query(COUNT_QUERY, params);
-
-    // Retourne les données et le nombre total d'ouvrages
-    return {
-      datas: datas,
-      count: countResult[0].total,
-    };
-  }
-
-  // Récupère un ouvrage spécifique d'un utilisateur
-  static async findOne({ users_id, works_id }) {
-    const FIND_ONE_WORK = `SELECT 
-        works.id AS works_id, 
-        works.name AS works_name,
-        works.edition AS works_edition,
-        works.type AS works_type,
-        works.format AS works_format,
-        volumes.id AS vol_id,
-        volumes.number AS vol_num,
-        volumes.title AS vol_title,
-        volumes.isbn AS vol_isbn,
-        volumes.summary AS vol_summary,
-        volumes.status AS vol_status,
-        volumes.created_at AS created_at,
-        volumes.creator_visibility AS creator_visibility,
-        users.id AS user_id,
-        COALESCE(GROUP_CONCAT(DISTINCT authors.name SEPARATOR ','), 'Inconnu') AS authors_name,
-        medias.url AS url_media
-      FROM works
-      LEFT JOIN volumes ON volumes.works_id = works.id
-      LEFT JOIN users ON volumes.users_id = users.id
-      LEFT JOIN volumes_authors ON volumes_authors.volumes_id = volumes.id
-      LEFT JOIN authors ON authors.id = volumes_authors.authors_id
-      LEFT JOIN medias ON medias.volumes_id = volumes.id
-      INNER JOIN shelfs ON shelfs.volumes_id = volumes.id AND shelfs.users_id = ?
+    works.id AS works_id, 
+    works.name AS works_name,
+    works.edition AS works_edition,
+    works.type AS works_type,
+    works.format AS works_format,
+    volumes.id AS vol_id,
+    volumes.number AS vol_num,
+    volumes.title AS vol_title,
+    volumes.isbn AS vol_isbn,
+    volumes.summary AS vol_summary,
+    volumes.status AS vol_status,
+    volumes.created_at AS created_at,
+    volumes.creator_visibility AS creator_visibility,
+    shelfs.status AS vol_status_user,
+    COALESCE(GROUP_CONCAT(DISTINCT authors.name SEPARATOR ','), 'Inconnu') AS authors_name,
+    medias.url AS url_media
+FROM works
+LEFT JOIN volumes ON volumes.works_id = works.id
+LEFT JOIN volumes_authors ON volumes_authors.volumes_id = volumes.id
+LEFT JOIN authors ON authors.id = volumes_authors.authors_id
+LEFT JOIN medias ON medias.volumes_id = volumes.id
+INNER JOIN shelfs ON shelfs.volumes_id = volumes.id AND shelfs.users_id = ?
 WHERE works.id = ?
-      GROUP BY volumes.id, works.id, users.id, medias.url
-      ORDER BY vol_num;`; // Trie par numéro de volume
+GROUP BY volumes.id, works.id, medias.url
+ORDER BY volumes.number;`; // Trie par numéro de volume
     return await pool.query(FIND_ONE_WORK, [users_id, works_id]);
   }
 
